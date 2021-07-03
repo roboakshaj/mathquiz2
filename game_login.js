ecfeebcd6180eebe1b7e434e674c1069cd8c1ea5
roboakshaj <86538821+roboakshaj@users.noreply.github.com>
2021-07-03 10:52:57 -0400

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,7 @@
+function adduser(){
+    player_1=document.getElementById("player1_name_input").value;
+    player_2=document.getElementById("player2_name_input").value;
+    localStorage.setItem("player_1", player_1);
+    localStorage.setItem("player_2", player_2);
+     window.location="game_page.html"
+}    
